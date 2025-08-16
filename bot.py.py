d9a591d7baa6e4fce7cd2792e1e3e029501e58d2
Bod1ev <bogdan.perepel11@gmail.com>
2025-08-16 18:31:46 +0300

    bot.py

--- bot.py.py
+++ bot.py.py
@@ -1,11 +1,12 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.environ["8452226570:AAHjrpEYZrmz7EO8aZLdqZonKTFzH7R1J9Y"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привіт! Я твій бот!")
 
-app = ApplicationBuilder().token("8452226570:AAHjrpEYZrmz7EO8aZLdqZonKTFzH7R1J9Y").build()
-
+app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
-
 app.run_polling()
